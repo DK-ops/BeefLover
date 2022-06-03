70d3d1e9ab09f9f8112d8df97e5842798d81c42c
DK-ops <321Andika@gmail.com>
2022-06-03 20:39:51 +0700

    dah di gabungin

--- app/src/main/java/com/mobilepro/beeflover/fragment/FragmentHome.java
+++ app/src/main/java/com/mobilepro/beeflover/fragment/FragmentHome.java
@@ -1,27 +1,36 @@
 package com.mobilepro.beeflover.fragment;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.SearchView;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.mobilepro.beeflover.R;
 import com.mobilepro.beeflover.etc.SliderAdapter;
+import com.mobilepro.beeflover.kategori.BumbuDapur;
+import com.mobilepro.beeflover.kategori.DagingOlahan;
+import com.mobilepro.beeflover.kategori.DagingSteak;
+import com.mobilepro.beeflover.kategori.PaketDaging;
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
-public class FragmentHome extends Fragment {
-
+public class FragmentHome extends Fragment implements View.OnClickListener {
     SliderView sliderView;
     int[] images = {R.drawable.carousel_1,
     R.drawable.carousel_2,
     R.drawable.carousel_3};
+    CardView cvDS, cvDO, cvPD, cvBD;
 
 
     @Override
@@ -31,6 +40,15 @@ public class FragmentHome extends Fragment {
         View view1 = inflater.inflate(R.layout.fragment_home,container,false);
 
         sliderView = view1.findViewById(R.id.carouselH);
+        cvDS = view1.findViewById(R.id.cvdagingsteak);
+        cvDO = view1.findViewById(R.id.cvdagingolahan);
+        cvPD = view1.findViewById(R.id.cvpaketdaging);
+        cvBD = view1.findViewById(R.id.cvbumbudapur);
+        cvDS.setOnClickListener(this::onClick);
+        cvDO.setOnClickListener(this::onClick);
+        cvPD.setOnClickListener(this::onClick);
+        cvBD.setOnClickListener(this::onClick);
+
         SliderAdapter sliderAdapter = new SliderAdapter(images);
         sliderView.setSliderAdapter(sliderAdapter);
         sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
@@ -50,6 +68,34 @@ public class FragmentHome extends Fragment {
             public boolean onQueryTextChange(String newText) {
                 return false;
             }
-        }); return view1;
+        });
+
+        return view1;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.cvdagingsteak:
+                Intent intent = new Intent(getActivity(), DagingSteak.class);
+                this.startActivity(intent);
+                break;
+
+            case R.id.cvdagingolahan:
+                Intent intent1 = new Intent(getActivity(), DagingOlahan.class);
+                this.startActivity(intent1);
+                break;
+            case R.id.cvpaketdaging:
+                Intent intent2 = new Intent(getActivity(), PaketDaging.class);
+                this.startActivity(intent2);
+                break;
+            case R.id.cvbumbudapur:
+                Intent intent3 = new Intent(getActivity(), BumbuDapur.class);
+                this.startActivity(intent3);
+                break;
+
+        }
     }
 }
---
+++ app/src/main/java/com/mobilepro/beeflover/kategori/PaketDaging.java
@@ -0,0 +1,16 @@
+package com.mobilepro.beeflover.kategori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mobilepro.beeflover.R;
+
+public class PaketDaging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kat_paket_daging);
+    }
+}
