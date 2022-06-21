487221f75e63ef3c3b81dede448308e4fa502dff
Adit Rizki <rizkiadit23@gmail.com>
2022-06-22 00:19:01 +0700

    hehe2

--- app/src/main/java/com/mobilepro/beeflover/validation/LoginPage.java
+++ app/src/main/java/com/mobilepro/beeflover/validation/LoginPage.java
@@ -1,21 +1,67 @@
 package com.mobilepro.beeflover.validation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobilepro.beeflover.MainActivity;
 import com.mobilepro.beeflover.R;
 
 public class LoginPage extends AppCompatActivity {
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    String email, password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        inputEmail = findViewById(R.id.EditTextLoginEmail);
+        inputPassword = findViewById(R.id.EditTextLoginPassword);
+        btnLogin = findViewById(R.id.btn_login_masuk);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cekLogin();
+            }
+        });
     }
+
+    private void cekLogin() {
+        email = inputEmail.getText().toString();
+        password = inputPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email,password )
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(LoginPage.this, "Login Berhasil", Toast.LENGTH_LONG).show();
+                            Intent masuk = new Intent( LoginPage.this, MainActivity.class);
+                            startActivity(masuk);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginPage.this, "Login Gagal, Email atau Password anda salah", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
     public void onClickGoToDaftar(View view) {
         Intent daftar = new Intent(LoginPage.this, RegisterPage.class );
         startActivity(daftar);
@@ -24,10 +70,5 @@ public class LoginPage extends AppCompatActivity {
     public void onClickGoToLupaPassword(View view) {
         Intent lupapw = new Intent(LoginPage.this, ForgotPassword.class );
         startActivity(lupapw);
-    }
-
-    public void onClickbtnLoginMasuk(View view) {
-        Intent masuk = new Intent( LoginPage.this, MainActivity.class);
-        startActivity(masuk);
     }
 }
--- app/src/main/java/com/mobilepro/beeflover/validation/RegisterPage.java
+++ app/src/main/java/com/mobilepro/beeflover/validation/RegisterPage.java
@@ -1,46 +1,72 @@
 package com.mobilepro.beeflover.validation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobilepro.beeflover.MainActivity;
 import com.mobilepro.beeflover.R;
 
 public class RegisterPage extends AppCompatActivity {
+    EditText username, Email, nomor, password1, password2;
+    String nama, email, nohp, pass1, pass2;
+    Button daftar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        username = findViewById(R.id.EditTextRegistUsername);
+        Email = findViewById(R.id.EditTextRegistEmail);
+        nomor = findViewById(R.id.EditTextRegistPhone);
+        password1 = findViewById(R.id.EditTextRegistPassword);
+        password2 = findViewById(R.id.EditTextRegistConfirmPassword);
+        daftar = findViewById(R.id.btnDaftar);
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrasi();
+            }
+        });
     }
+
+    private void registrasi() {
+        nama = Email.getText().toString();
+        pass1 = password2.getText().toString();
+        pass2 = password2.getText().toString();
+
+            mAuth.createUserWithEmailAndPassword(nama, pass2)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(RegisterPage.this, "Registrasi Berhasil", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                                startActivity(intent);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(RegisterPage.this, "Registrasi Gagal", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+    }
+
     public void onClickRegistBackToLogin(View view) {
         Intent bckLogin = new Intent(RegisterPage.this, LoginPage.class );
         startActivity(bckLogin);
-    }
-
-    public void daftarakun(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-
-        EditText regpassword= (EditText) findViewById(R.id.EditTextRegistPassword);
-        EditText regConfirmPass = (EditText) findViewById(R.id.EditTextRegistConfirmPassword);
-
-        String pass = regpassword.getText().toString();
-        String conpass = regConfirmPass.getText().toString();
-
-        if (pass.equals("")) {
-            Toast.makeText(this, "Password cannot be null", Toast.LENGTH_SHORT).show();
-        } else {
-            if (!pass.equals(conpass)) {
-                Toast.makeText(this, "Password not same with confirm password", Toast.LENGTH_SHORT).show();
-            } else {
-                startActivity(intent);
-
-            }
-        }
     }
 }
