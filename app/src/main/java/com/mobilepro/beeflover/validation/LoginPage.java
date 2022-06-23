f84673c46495db99f9cfe6ea2af932223be00e86
Adit Rizki <rizkiadit23@gmail.com>
2022-06-23 10:06:34 +0700

    hehe2

--- app/src/main/java/com/mobilepro/beeflover/validation/LoginPage.java
+++ app/src/main/java/com/mobilepro/beeflover/validation/LoginPage.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,7 @@ public class LoginPage extends AppCompatActivity {
     Button btnLogin;
     String email, password;
     private FirebaseAuth mAuth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,22 +46,31 @@ public class LoginPage extends AppCompatActivity {
         email = inputEmail.getText().toString();
         password = inputPassword.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email,password )
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(LoginPage.this, "Login Berhasil", Toast.LENGTH_LONG).show();
-                            Intent masuk = new Intent( LoginPage.this, MainActivity.class);
-                            startActivity(masuk);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(LoginPage.this, "Login Gagal, Email atau Password anda salah", Toast.LENGTH_SHORT).show();
+        if(TextUtils.isEmpty(email)|| TextUtils.isEmpty(password)){
+            Toast.makeText(LoginPage.this, "Masukan Email dan Password", Toast.LENGTH_LONG).show();
+        }else if(!email.matches(emailPattern)){
+            inputEmail.setError("Email Salah");
+            Toast.makeText(LoginPage.this, "Email salah", Toast.LENGTH_LONG).show();
+        }else if (password.length()<6){
+            inputPassword.setError("Password Salah");
+            Toast.makeText(LoginPage.this, "Please enter valid password", Toast.LENGTH_LONG).show();
+        }else {
+            mAuth.signInWithEmailAndPassword(email,password )
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(LoginPage.this, "Login Berhasil", Toast.LENGTH_LONG).show();
+                                Intent masuk = new Intent( LoginPage.this, MainActivity.class);
+                                startActivity(masuk);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginPage.this, "Login Gagal, Email atau Password anda salah", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
-
+                    });
+        }
     }
 
     public void onClickGoToDaftar(View view) {
